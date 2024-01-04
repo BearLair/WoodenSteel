2c29dab068ff559d2281865c452b876e5aa039d2
nick <nick.git@cryona.ru>
2024-01-04 17:25:15 +0300

    Универсальный bat

--- zip_make_pack.bat
+++ zip_make_pack.bat
@@ -1,6 +1,6 @@
 
 "C:\Program Files\WinRAR\WinRar.exe" a -afzip -m0 -u -as WoodenSteel.mpk Content Header.xml License.txt ReadMe.txt
 
-copy WoodenSteel.mpk "C:\Users\Nick\Documents\AtomicTorchStudio\CryoFall\Mods\"
+copy WoodenSteel.mpk "C:\Users\%USERNAME%\Documents\AtomicTorchStudio\CryoFall\Mods\"
 
 timeout 60
